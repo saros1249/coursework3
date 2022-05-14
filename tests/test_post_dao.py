9c7a6c6c9d62da4c22dd4000b9714b97936637f7
saros1249 <saros1249@gmail.com>
2022-05-14 11:33:24 +0800

    Курсовая-3 Добавлен 2 тест для поста по ID.

--- tests/test_post_dao.py
+++ tests/test_post_dao.py
@@ -2,12 +2,12 @@ import pytest
 
 from app.posts.dao.posts_dao import PostsDAO
 
+
 class TestPostsDAO:
 
     @pytest.fixture
     def posts_dao(self):
         return PostsDAO('data/data.json')
-
 
     def test_get_posts_all_check_type(self, posts_dao):
         posts = posts_dao.get_posts_all()
@@ -25,9 +25,13 @@ class TestPostsDAO:
 
     def test_post_by_id_chek_type(self, posts_dao):
         for i in range(len(posts_dao.get_posts_all())):
-            post = posts_dao.post_by_id(i+1)
+            post = posts_dao.post_by_id(i + 1)
             assert type(post) == dict, 'Каждый пост должен быть словарём'
 
-
-
-
+    def test_gpost_by_id_has_keys(self, posts_dao):
+        posts = posts_dao.get_posts_all()
+        for i in range(len(posts)):
+            first_post = posts_dao.post_by_id(i + 1)
+            keys_expected = {"poster_name", "poster_avatar", "pic", "content", "views_count", "likes_count", "pk"}
+            first_post_keys = set(first_post.keys())
+            assert first_post_keys == keys_expected, 'Полученные ключи не верны'
